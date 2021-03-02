018122621c92cd1dfc0c7c0296fd2d68bd0973cd
zakinao <zakinaororo@gmail.com>
2021-03-02 11:18:16 +0000

    finish ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,15 @@
 class BookCommentsController < ApplicationController
     def create
-      book = Book.find(params[:book_id])
+      @book = Book.find(params[:book_id])
       comment = current_user.book_comments.new(book_comment_params)
-      comment.book_id = book.id
+      comment.book_id = @book.id
       comment.save
-      redirect_back(fallback_location: root_path)
     end
     
     def destroy
       @book = Book.find(params[:book_id])
       book_comment = @book.book_comments.find(params[:id])
       book_comment.destroy
-      redirect_back(fallback_location: root_path)
     end
     
     private
